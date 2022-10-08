12b0ce1dca4b6e6b9210cd4ee1127067869e980d
Bilal-01 <mbilalaziz.01@gmail.com>
2022-10-08 15:49:37 +0500

    Home page updated

--- src/components/ExploreBlogsSection/ExploreBlogsSection.jsx
+++ src/components/ExploreBlogsSection/ExploreBlogsSection.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import Card from '../Card/Card';
 import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import exploreImg from './../../assets/bg-home1.jpeg';
 import './explore.css';
 
 function ExploreBlogsSection(){
     return(
         <>
             <div className="explore-blogs-container">
-                <Card 
-                    icon={<AutoStoriesIcon fontSize='large' color='success' />}
-                    heading="Title"
-                    description="orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                />
-                <Card 
-                    icon={<AutoStoriesIcon fontSize='large' color='success' />}
-                    heading="Title"
-                    description="orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                />
+                <div className="explore-card">
+                    <div className="explore-img-container">
+                        <img className="explore-img" src={exploreImg} alt="" />
+                    </div>
+                    <div className='text-container'>
+                        <div className="text">
+                            <h3 className="text-heading">
+                                Explore
+                            </h3>
+                            <p className='text-content'>
+                                Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem.
+                            </p>
+
+                            
+                            <h3 className="text-heading">
+                                Explore
+                            </h3>
+                            <p className='text-content'>
+                                Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
 
