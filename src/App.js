e9e27273581cddd2505584f5992417c0a1df9dbf
= <=>
2022-10-08 15:46:33 +0500

    contact page created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
 import {BrowserRouter as Router, Switch, Route, Routes} from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
       </Router>
     </>
