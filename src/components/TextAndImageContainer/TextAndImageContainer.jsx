ff03be831828595c7e91e3edd32787dfb524c283
Bilal-01 <mbilalaziz.01@gmail.com>
2022-10-11 11:54:21 +0500

    Home page: animation + curved bottom

---
+++ src/components/TextAndImageContainer/TextAndImageContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Card, CardMedia, CardActions} from '@mui/material';
+
+function TextAndImageContainer(props){
+    const styles={
+        
+    }
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,7 @@
-export {}
+export {default as Header} from './Header/Header';
+export {default as Navbar} from './Navbar/Navbar';
+export {default as Footer} from './Footer/Footer';
+export {default as ExploreBlogsSection} from './ExploreBlogsSection/ExploreBlogsSection';
+export {default as CardContainer} from './CardContainer/CardContainer';
+export {default as Card} from './Card/Card';
+export {default as BlogContainer} from './BlogContainer/BlogContainer';
