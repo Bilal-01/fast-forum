0f2bbf83813670db88abab915e1c4048b3076bce
SabahMawani <sabahmawani@gmail.com>
2022-11-06 19:00:02 +0500

    Blog page updated

--- src/components/BlogContainer/BlogContainer.jsx
+++ src/components/BlogContainer/BlogContainer.jsx
@@ -1,80 +1,80 @@
+import { Fab } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 import React from 'react';
 import exploreImg from './../../assets/bg-home1.jpeg';
+import BImg from './../../assets/blog-1.jpg';
 import './blogcont.css';
 
 function BlogContainer()
 {
     return(
         <>
-            <div className="blog-container">
-                <article class="left-to-right-cont">
-                    <header className='text-container'>
-                        <h2>
-                            "Why Bilal is an idiot"
-                        </h2>
-                        <div className='Author-container'>
+            <div className="b-container">
+                <img className='bbg-img' src={BImg} alt="background" />
+                <div className="bimg-shadow"></div>
+            </div>
+            <div className='b-main'>
+                <div className="blog-container">
+                    <article class="left-to-right-cont">
+                        <header className='text-container'>
+                            <h2>
+                                "Lorem ipsum"
+                            </h2>
                             <div>
                                 Unknown 
                             </div>
                             <div>
                                 9th October 2022
                             </div>
+                        </header>
+                        <div>
+                            Lorem ipsum dolor sit amet. Id provident consequatur et sint minus sit quam quia et dolores itaque. Vel cumque voluptas cum nisi Quis ad eius impedit. Et nobis saepe aut impedit maiores qui suscipit eligendi nam magni quam ut explicabo internos sit autem maxime sed sequi cumque. Et quidem voluptatem est reiciendis delectus sed laborum nisi non molestias eligendi At ipsam dolore.
                         </div>
-                    </header>
-                    <div>
-                        Because he's a cheating alligator, a dumb alligator, a scary alligator,Mean alligator,Big Alligator,Idiot Alligator,Annoying Alligator,Ginormous Gator,Matlabi Alligator,Selfish Alligator,Bad Alligator,Stupid Alligator,Unkind Alligator,Boring Gator,Old Alligator,Short Alligator,Irritating Alligator,Infuriating Alligator,Exasperating Alligator,Thickheaded Alligator,Goofball Gator,Git Gator
-                    </div>
-                    <div>
-                        <img className="explore-img-container" src={exploreImg} alt="" />
-                    </div>
-                </article>
-            </div>
-            <div className="blog-container">
-                <article class="left-to-right-cont">
-                    <header className='text-container'>
-                        <h2>
-                            "Why Bilal is an idiot"
-                        </h2>
-                        <div className='Author-container'>
+                    </article>
+                </div>
+                <div className="blog-container">
+                    <article class="left-to-right-cont">
+                        <header className='text-container'>
+                            <h2>
+                                "Lorem ipsum"
+                            </h2>
+                            <div>
+                                Unknown 
+                            </div>
+                            <div>
+                                9th October 2022
+                            </div>                   
+                        </header>
+                        <div className='content-container'>
+                            Lorem ipsum dolor sit amet. Id provident consequatur et sint minus sit quam quia et dolores itaque. Vel cumque voluptas cum nisi Quis ad eius impedit. Et nobis saepe aut impedit maiores qui suscipit eligendi nam magni quam ut explicabo internos sit autem maxime sed sequi cumque. Et quidem voluptatem est reiciendis delectus sed laborum nisi non molestias eligendi At ipsam dolore.
+                        </div>
+                    </article>
+                </div>
+                <div className="blog-container">
+                    <article class="left-to-right-cont">
+                        <header className='text-container'>
+                            <h2>
+                                "Lorem ipsum"
+                            </h2>
                             <div>
                                 Unknown 
                             </div>
                             <div>
                                 9th October 2022
                             </div>
+                        </header>
+                        <div className='content-container'>
+                            Lorem ipsum dolor sit amet. Id provident consequatur et sint minus sit quam quia et dolores itaque. Vel cumque voluptas cum nisi Quis ad eius impedit. Et nobis saepe aut impedit maiores qui suscipit eligendi nam magni quam ut explicabo internos sit autem maxime sed sequi cumque. Et quidem voluptatem est reiciendis delectus sed laborum nisi non molestias eligendi At ipsam dolore.
                         </div>
-                    </header>
-                    <div>
-                        <img className="explore-img-container" src={exploreImg} alt="" />
-                    </div>
-                    <div className='content-container'>
-                        Because he's a cheating alligator, a dumb alligator, a scary alligator,Mean alligator,Big Alligator,Idiot Alligator,Annoying Alligator,Ginormous Gator,Matlabi Alligator,Selfish Alligator,Bad Alligator,Stupid Alligator,Unkind Alligator,Boring Gator,Old Alligator,Short Alligator,Irritating Alligator,Infuriating Alligator,Exasperating Alligator,Thickheaded Alligator,Goofball Gator,Git Gator
-                    </div>
-                </article>
+                    </article>
+                </div>
+                <div className='b-add'>
+                <Fab classname='b-add' color='primary' aria-label='add blog'>
+                <AddIcon />
+            </Fab>
+                </div>
             </div>
-            <div className="blog-container">
-                <article class="left-to-right-cont">
-                    <header className='text-container'>
-                        <h2>
-                            "Why Bilal is an idiot"
-                        </h2>
-                        <div className='Author-container'>
-                            <div>
-                                Unknown 
-                            </div>
-                            <div>
-                                9th October 2022
-                            </div>
-                        </div>
-                    </header>
-                    <div>
-                        Because he's a cheating alligator, a dumb alligator, a scary alligator,Mean alligator,Big Alligator,Idiot Alligator,Annoying Alligator,Ginormous Gator,Matlabi Alligator,Selfish Alligator,Bad Alligator,Stupid Alligator,Unkind Alligator,Boring Gator,Old Alligator,Short Alligator,Irritating Alligator,Infuriating Alligator,Exasperating Alligator,Thickheaded Alligator,Goofball Gator,Git Gator
-                    </div>
-                    <div>
-                        <img className="explore-img-container" src={exploreImg} alt="" />
-                    </div>
-                </article>
-            </div>
+            
         </>
         
         
