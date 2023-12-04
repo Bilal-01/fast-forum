48d25e3c92a9cb590293316b32ae882cf92698fe
Bilal-01 <mbilalaziz.01@gmail.com>
2023-12-04 22:11:01 +0500

    about us fixed

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Navbar, Footer, TextAndImageContainer, UserCard } from '../../components';
 import aboutImg from '../../assets/about-us.jpeg';
-import alligator from '../../assets/alligator.png';
-import panda from '../../assets/panda.png';
-import dino from '../../assets/dino.png';
-import bilal from '../../assets/bilal.jpg';
+import userIcon from '../../assets/user-icon.png';
 
 import './about.css';
 
@@ -14,9 +11,9 @@ function About(){
             <Navbar />
             <TextAndImageContainer width="70%" height="70vh" src={aboutImg} />
             <div className='user-card-container'>
-                <UserCard src={bilal} name="Bilal Aziz" description="" />
-                <UserCard src={panda} name="Maryam Siddiqui" description="" />
-                <UserCard src={dino} name="Sabah Mawani" description="" />
+                <UserCard src={userIcon} name="Bilal Aziz" description="" />
+                <UserCard src={userIcon} name="Maryam Siddiqui" description="" />
+                <UserCard src={userIcon} name="Imtiaz" description="" />
             </div>
             <Footer />
         </>
